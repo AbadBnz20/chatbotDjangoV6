74f4c4eab6bf0cec13e9f4321c9c9dd2eec2e17f
AbadBnz23 <benizabadchat@gmail.com>
2023-06-06 16:19:45 -0400

    eliminado de todo

--- api/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Programmer
-
-
-class ProgrammerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Programmer
-        # fields = ('fullname', 'nickname')
-        fields = '__all__'
